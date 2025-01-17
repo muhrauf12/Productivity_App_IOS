383deea828728b9b23ede82da3a6ba94ce80643e
Rauf M <muhrauf@umich.edu>
2025-01-17 02:46:19 -0500

    Initial Commit

---
+++ tech/techApp.swift
@@ -0,0 +1,17 @@
+//
+//  techApp.swift
+//  tech
+//
+//  Created by Muhammad Rauf on 1/17/25.
+//
+
+import SwiftUI
+
+@main
+struct techApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ techTests/techTests.swift
@@ -0,0 +1,17 @@
+//
+//  techTests.swift
+//  techTests
+//
+//  Created by Muhammad Rauf on 1/17/25.
+//
+
+import Testing
+@testable import tech
+
+struct techTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
